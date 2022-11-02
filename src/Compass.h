2d02a54c337c5868e79499406d898ebdc13d3936
Jamerson <102273220+jamersonm@users.noreply.github.com>
2022-11-02 11:34:29 -0300

    Add files via upload

--- src/Compass.h
+++ src/Compass.h
@@ -5,42 +5,53 @@
 #include <MPU6050_light.h>
 
 MPU6050 mpu(Wire);
+float yaw = 0;
+int divider;
+unsigned long timer = millis();
 
-class Compass {
-private:
-    float first;
-    float last;
-public:
-    Compass(){configureCompass();}
-    ~Compass(){}
+void calibrateCompass()
+{
+    mpu.calcOffsets(true, true);
+}
 
-    void calibrateCompass();
-    void configureCompass();
-    float readCompass();
-    void printCompass();
-};
-
-void Compass::configureCompass()
+void configureCompass()
 {
     Wire.begin();
     mpu.begin();
     calibrateCompass();
 }
 
-void Compass::calibrateCompass()
-{
-    mpu.calcOffsets(true,true); // gyro and accelero
-}
-
-float Compass::readCompass()
+void loopCompass()
 {
     mpu.update();
-    return (mpu.getAngleZ());
+    if((millis() - timer) < 10) // yaw = yaw + yaw_rate*dt
+    {
+        yaw = mpu.getAngleZ();
+
+        //set interval 0 <= angle < 360
+        if(yaw >= 360)
+        {
+            divider = yaw / 360;
+            for(int i = 0; i < divider; i++)
+            yaw = yaw - 360;
+        }
+        else if(yaw < 0)
+        {
+            divider = yaw / 360;
+            yaw = yaw + 360;
+            for(int i = 0; i < divider; i++)
+            yaw = yaw + 360;
+        }
+    }
+    if((millis() - timer) > 20)
+        timer = millis();
 }
 
-void Compass::printCompass()
+void printCompass()
 {
-    Serial.println(readCompass());
+    Serial.print("Compass: ");
+    Serial.print(yaw);
+    Serial.println("°");
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 #include "Compass.h"
 
+unsigned long timer_print = millis();
+
 void setup() {
   Serial.begin(9600);
-
+  configureCompass(); //initialize  the MPU 6050 sensor
 }
 
 void loop() {
-
+  loopCompass(); // MPU sensor start getting yaw
+  if((millis() - timer_print) > 5000) // after 5 segs
+  {
+    printCompass(); // print yaw
+    timer_print = millis();
+  }
 }
